0b0c94f22c447d263aa944715df55baf5c798ff0
Leosss <leonardomartinezubal3@gmail.com>
2017-01-22 00:05:16 +0100

    Datos user guardados al hacer login, y no se guardan si ya existen.

--- app/src/main/java/com/leosssdroid/tastycocktails/LoginActivity.java
+++ app/src/main/java/com/leosssdroid/tastycocktails/LoginActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +22,11 @@ import com.google.firebase.auth.FacebookAuthCredential;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
@@ -28,20 +35,29 @@ public class LoginActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference usersRef = mDatabase.child("users");
     private LoginButton loginButton;
+    private boolean userFound = false;
+    public UserTasty userTasty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //referencia de la base de datos de Firebase
+        System.out.println(mDatabase);
+
         callbackManager = CallbackManager.Factory.create();
+
         loginButton = (LoginButton)findViewById(R.id.loginButtonFb);
-        loginButton.setReadPermissions(Arrays.asList("email","user_friends"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 handleFacebookAccessToken(loginResult.getAccessToken());
+                System.out.println(loginResult.getAccessToken().getUserId()+"    ----> LEO");
+                creaUserTasty(Profile.getCurrentProfile());
             }
 
             @Override
@@ -61,12 +77,49 @@ public class LoginActivity extends AppCompatActivity {
         firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null){
-                    goMainActivity();
+                    usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                                UserTasty userSnapshot = postSnapshot.getValue(UserTasty.class);
+                                if(userSnapshot == null){
+                                    break;
+                                }else if(userSnapshot.getIdFacebbok().equals(userTasty.getIdFacebbok())){
+                                    System.out.println("USER FOUND!!!!!!!!!!!!!!!");
+                                    userFound = true; //auth != null
+                                    break;
+                                }
+                            }
+
+                            if(!userFound){
+                                System.out.println("USER NOT FOUND!!!!!!!!!!!!!!!");
+                                writeNewuser(userTasty);
+                            }
+
+                            goMainActivity();
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
                 }
             }
         };
+    }
+
+    private void creaUserTasty(Profile currentProfile) {
+        userTasty = new UserTasty();
+        userTasty.setIdFacebbok(currentProfile.getId());
+        userTasty.setName(currentProfile.getName());
+        userTasty.setFirstName(currentProfile.getFirstName());
+        userTasty.setLastName(currentProfile.getLastName());
+        userTasty.setPicture(currentProfile.getProfilePictureUri(128,128).toString()); //ojo! este dato es android.net.Uri que es diferente a java.net.URI
+        userTasty.setDescription("Sin descripción");
     }
 
     private void handleFacebookAccessToken(AccessToken accessToken) {
@@ -79,6 +132,11 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void writeNewuser(UserTasty userTasty){
+        System.out.println("SUBIENDO BASE DE DATOS --->"+userTasty.getIdFacebbok());
+        usersRef.child(userTasty.getIdFacebbok()).setValue(userTasty);
     }
 
     private void goMainActivity() {
@@ -97,6 +155,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         firebaseAuth.addAuthStateListener(firebaseAuthListener);
+
     }
 
     @Override
---
+++ app/src/main/java/com/leosssdroid/tastycocktails/UserTasty.java
@@ -0,0 +1,75 @@
+package com.leosssdroid.tastycocktails;
+
+import android.net.Uri;
+
+/**
+ * Created by Leosss on 21/01/2017.
+ */
+
+public class UserTasty {
+    private String idFacebbok, name, firstName, lastName, description, picture;
+
+
+    public UserTasty() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public UserTasty(String idFacebbok, String picture, String description, String lastName, String firstName, String name) {
+        this.idFacebbok = idFacebbok;
+        this.picture = picture;
+        this.description = description;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.name = name;
+    }
+
+    //GETTERS Y SETTERS
+
+    public String getIdFacebbok() {
+        return idFacebbok;
+    }
+
+    public void setIdFacebbok(String idFacebbok) {
+        this.idFacebbok = idFacebbok;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
