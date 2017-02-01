0e4caca994b58ec1d6ebce6a47f633967a07373c
Leosss <leonardomartinezubal3@gmail.com>
2017-02-01 14:47:36 +0100

    Cambios en el item del recyclerview en inicio: agregamos la foto circular del usuario que la ha subido.

--- app/src/main/java/com/leosssdroid/tastycocktails/Adapters/RecipesAdapter.java
+++ app/src/main/java/com/leosssdroid/tastycocktails/Adapters/RecipesAdapter.java
@@ -10,11 +10,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.leosssdroid.tastycocktails.Clases.Recetas;
+import com.leosssdroid.tastycocktails.Clases.UserTasty;
 import com.leosssdroid.tastycocktails.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Leosss on 30/01/2017.
@@ -36,11 +44,27 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesV
     }
 
     @Override
-    public void onBindViewHolder(RecipesViewHolder holder, int position) {
-        Recetas rec = recetas.get(position);
+    public void onBindViewHolder(final RecipesViewHolder holder, int position) {
+        final Recetas rec = recetas.get(position);
+
         Glide.with(context).load(rec.getPicture()).centerCrop().into(holder.imagen_receta);
+        final DatabaseReference refNombre = FirebaseDatabase.getInstance().getReference().child("users").child(rec.getIdUsuario());
+        refNombre.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserTasty utasty = dataSnapshot.getValue(UserTasty.class);
+                Glide.with(context).load(utasty.getPicture()).centerCrop().into(holder.userPic);
+                holder.nombre.setText("Subido por "+utasty.getName());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         holder.titulo.setText(rec.getNombre());
-        holder.nombre.setText("Subido por "+rec.getIdUsuario());
+        //holder.nombre.setText("Subido por "+rec.getIdUsuario());
     }
 
     @Override
@@ -51,13 +75,16 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesV
 
     class RecipesViewHolder extends RecyclerView.ViewHolder {
         private ImageView imagen_receta;
+        private CircleImageView userPic;
         private TextView titulo, nombre;
 
         public RecipesViewHolder(View itemView) {
             super(itemView);
             imagen_receta = (ImageView) itemView.findViewById(R.id.imagen_coctel_card);
+            userPic = (CircleImageView) itemView.findViewById(R.id.profileImageRecetaCard);
             titulo = (TextView) itemView.findViewById(R.id.titulo_card);
             nombre = (TextView) itemView.findViewById(R.id.nombre_user_card);
+
         }
 
         public ImageView getImagen_receta() {
